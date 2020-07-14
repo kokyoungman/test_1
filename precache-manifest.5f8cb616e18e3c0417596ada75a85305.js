a20d3f9d8c72a1c7bfe3e2fb997ad3cc153e270e
kokyoungman <kokyoungman@gmail.com>
2020-07-14 17:46:19 +0900

    Updates

---
+++ precache-manifest.5f8cb616e18e3c0417596ada75a85305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714c6911922d98c051425acdd28cb285",
+    "url": "/test_1/index.html"
+  },
+  {
+    "revision": "82156e4461652e3a9e02",
+    "url": "/test_1/static/css/main.b2993846.chunk.css"
+  },
+  {
+    "revision": "a133f8da8ec04a864f55",
+    "url": "/test_1/static/js/2.e2b53bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_1/static/js/2.e2b53bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82156e4461652e3a9e02",
+    "url": "/test_1/static/js/main.728ce121.chunk.js"
+  },
+  {
+    "revision": "73d829c84c2317620a48",
+    "url": "/test_1/static/js/runtime-main.705bc313.js"
+  }
+]);
